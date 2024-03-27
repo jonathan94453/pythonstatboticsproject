184bfd74ae2603e81b9bcb45aaf96beab31d9a68
Jonathan94453 <cino.jonathan@gmail.com>
2024-03-27 14:33:26 -0500

    got api and tkinter app working

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import statbotics 
+import tkinter as tk 
 
+root = tk.Tk()
 stats = statbotics.Statbotics() 
 
-team = int(input()) 
+root.configure(background="black") 
+root.title("Statbotics APP") 
+
+entry = tk.Entry(root, width=40, ) 
+entry.pack(pady=10) 
 
 
+json_response = stats.get_team(4329) 
 
- 
+teamepa = json_response.get("norm_epa_recent") 
+
+teamname = json_response.get("name") 
 
 
-print(stats.get_team_events(team, 2024) 
+# adding the main loop function on the tk object which allows the app to run with the changes made 
+root.mainloop() 
