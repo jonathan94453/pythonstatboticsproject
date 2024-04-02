29614bcc9ffa8500c6fc9003ea4f30858ba7e50e
Jonathan <cino.jonathan@gmail.com>
2024-04-02 11:23:27 -0500

    made a page switch as well as re added functionality to team search for welcome page

--- app.py
+++ app.py
@@ -1,39 +1,54 @@
-
-import tkinter as tk 
-from tkinter import font
-from typing import Tuple 
+import tkinter as tk
+import customtkinter as ctk
 import api 
-import dataprocessing 
-import customtkinter as ctk 
 
- 
+import dataprocessing 
 class EPAPage(ctk.CTkFrame):
     def __init__(self, parent):
-        ctk.CTkFrame.__init__(self, parent) 
-       
+        super().__init__(parent)
+        label1 = ctk.CTkLabel(self, text="Welcome to EPA Page")
+        label1.pack(pady=10, padx=10)
 
-        parent.geometry("900x900")
-        label1 = ctk.CTkLabel(parent, text="welcome to epa button") 
-        label1.pack(pady=10, padx=10) 
-
-
-class welcomepage(ctk.CTkFrame):
+class WelcomePage(ctk.CTkFrame):
     def __init__(self, parent):
-          super().__init__(parent)
-       
-        parent.geometry("800x800")  
-        
-        label1 = ctk.CTkLabel(parent, text="welcometomainpage")
-        label1.pack(pady=10, padx=10)  
-        def switchpage():
-            switch = EPAPage(parent)
-            switch.pack()
-        button1 = ctk.CTkButton(parent, text="EpaButton", command=switchpage())
+        super().__init__(parent)
+        self.label1 = ctk.CTkLabel(self, text="Welcome to Main Page")
+        self.label1.pack(pady=10, padx=10)
+        button1 = ctk.CTkButton(self, text="EPA Page", command=self.switch_page)
+        button1.place(x=450, y=100) 
+        button1.pack(padx=450, pady=100)
+
+        self.control = dataprocessing.appcontrol()
+        self.teamentrybox = ctk.CTkEntry(parent, width=80) 
+        self.teamentrybox.bind("<Return>", self.onenter)
+        self.teamentrybox.pack(padx=100, pady=200) 
+    def onenter(self, event):
+        self.control.on_enter(self.teamentrybox) 
+        response = self.control.getteamresponsename() 
+        self.label1.configure(text=response)
 
 
 
-if __name__ == "__main__":  
-    root = ctk.CTk() 
-    epa_page = welcomepage(root)
-    epa_page.pack() 
+
+
+
+
+
+
+    def switch_page(self):
+        # Hide the current page
+        self.pack_forget()
+        # Show the EPA page
+        epa_page.pack()
+
+if __name__ == "__main__":
+    root = ctk.CTk()
+    root.geometry("900x900")
+    # Create instances of the WelcomePage and EPAPage
+    welcome_page = WelcomePage(root)
+    epa_page = EPAPage(root)
+    
+    # Initially, pack the welcome page
+    welcome_page.pack()
+    
     root.mainloop()
