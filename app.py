101a657af005f3ca9919c8fc26ac34de2241e962
Jonathan <cino.jonathan@gmail.com>
2024-04-02 16:56:44 -0500

    frame switches

--- app.py
+++ app.py
@@ -6,22 +6,29 @@ import dataprocessing
 class EPAPage(ctk.CTkFrame):
     def __init__(self, parent):
         super().__init__(parent)
-        label1 = ctk.CTkLabel(self, text="Welcome to EPA Page")
-        label1.pack(pady=10, padx=10)
+        self.label1 = ctk.CTkLabel(parent, text="Welcome to EPA Page")
+        self.label1.place(x=100, y=100)
 
 class WelcomePage(ctk.CTkFrame):
-    def __init__(self, parent):
+    def __init__(self, parent, epa_page):
         super().__init__(parent)
-        self.label1 = ctk.CTkLabel(self, text="Welcome to Main Page")
+        
+        welcomefont = ctk.CTkFont(size=80)
+
+
+        self.label1 = ctk.CTkLabel(parent, text="Welcome to Main Page", font=welcomefont)
         self.label1.pack(pady=10, padx=10)
-        button1 = ctk.CTkButton(self, text="EPA Page", command=self.switch_page)
-        button1.place(x=450, y=100) 
-        button1.pack(padx=450, pady=100)
+        self.button1 = ctk.CTkButton(parent, text="EPA Page", command=self.switch_page)
+        self.button1.place(x=100, y=200)
 
         self.control = dataprocessing.appcontrol()
         self.teamentrybox = ctk.CTkEntry(parent, width=80) 
         self.teamentrybox.bind("<Return>", self.onenter)
-        self.teamentrybox.pack(padx=100, pady=200) 
+        self.teamentrybox.pack(padx=100, pady=100) 
+
+
+        self.epa_page = epa_page 
+
     def onenter(self, event):
         self.control.on_enter(self.teamentrybox) 
         response = self.control.getteamresponsename() 
@@ -37,16 +44,19 @@ class WelcomePage(ctk.CTkFrame):
 
     def switch_page(self):
         # Hide the current page
-        self.pack_forget()
+        self.label1.pack_forget()
+        self.button1.pack_forget()
+        self.teamentrybox.pack_forget()
         # Show the EPA page
-        epa_page.pack()
+        self.epa_page.pack() 
 
 if __name__ == "__main__":
     root = ctk.CTk()
     root.geometry("900x900")
+    root.title("4329 Scouting Integration Project")
     # Create instances of the WelcomePage and EPAPage
-    welcome_page = WelcomePage(root)
     epa_page = EPAPage(root)
+    welcome_page = WelcomePage(root, epa_page) 
     
     # Initially, pack the welcome page
     welcome_page.pack()
